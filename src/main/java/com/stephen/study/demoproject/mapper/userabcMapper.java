c51056d22bf77f061dc3bd207929dbd4a2e693af
李博文 <libowen@zzidc.com>
2020-07-28 19:03:50 +0800

    学习mybatis

---
+++ src/main/java/com/stephen/study/demoproject/mapper/userabcMapper.java
@@ -0,0 +1,9 @@
+package com.stephen.study.demoproject.mapper;
+
+import com.stephen.study.demoproject.model.userabc;
+
+public interface userabcMapper {
+    int insert(userabc record);
+
+    int insertSelective(userabc record);
+}
